e8444ebae091a68bc19bc6241d5dea303e9c5ddd
tvh0021 <trungha@my.unt.edu>
2023-12-23 00:59:11 -0500

    numba is annoying

--- popsom.py
+++ popsom.py
@@ -237,7 +237,9 @@ class map:
 		""" Gamma -- neighborhood function"""
 		# 2d distance between neuron c and the rest of the map
 		dist_2d = np.abs(m2Ds[c,:] - m2Ds)
-		rectangular_dist = np.max(dist_2d, axis=1) # 
+		rectangular_dist = np.zeros(dist_2d.shape[0], dtype=int)
+		for i in prange(dist_2d.shape[0]): # numba max does not have axis argument
+			rectangular_dist[i] = np.max(dist_2d[i,:])
   
 		#Define the Gaussian function to calculate the neighborhood function
 		def gauss(dist, A, sigma):
